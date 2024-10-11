08f01575f138f390562166ea470c21054574156f
Duc <thd1152015@gmail.com>
2024-10-11 14:19:18 +0700

    latest

--- src/app/@dtech/navbar/navbar.component.ts
+++ src/app/@dtech/navbar/navbar.component.ts
@@ -1,18 +1,11 @@
 // src/app/services/flowbite.service.ts
-import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
+@Component({
+  standalone: true,
+  selector: 'dtech-navbar',
+  templateUrl: './navbar.component.html',
 })
-export class FlowbiteService {
-  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
-
-  loadFlowbite(callback: (flowbite: any) => void) {
-    if (isPlatformBrowser(this.platformId)) {
-      import('flowbite').then((flowbite) => {
-        callback(flowbite);
-      });
-    }
-  }
+export class NavbarComponent {
+  constructor() {}
 }
