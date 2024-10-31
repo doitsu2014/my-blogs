849e1c14edd509498ae9001776e38964ba4b019a
Duc <thd1152015@gmail.com>
2024-10-31 10:35:37 +0700

    latest

--- src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css';
-	// import type { LayoutServerData } from './$types';
-	// import type { PageServerData } from './$types';
-	// export let layoutServerData: LayoutServerData;
-	// export let layoutServerData: PageServerData;
-</script>
-
-<slot></slot>
