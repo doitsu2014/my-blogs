6bd4be1efc53bc8c1a125f125ae184758c156c42
Duc <thd1152015@gmail.com>
2024-11-26 13:45:36 +0700

    latest

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,10 @@
-export default function AdminHomePage() {
-  return <div className="flex justify-center">a</div>;
+import { signIn, signOut, auth } from '@/auth';
+
+export default async function AdminHomePage() {
+  const session = await auth();
+  if (session) {
+    return <>{session.user?.email}</>;
+  }
+
+  return <></>;
 }
