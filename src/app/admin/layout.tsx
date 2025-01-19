1c4b4c13d195ba7b4aa61fe47292deccf9c2b7cd
ドイツ <thd1152015@gmail.com>
2025-01-19 14:29:59 +0700

    latest

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,39 +1,68 @@
 import { auth, signIn, signOut } from '@/auth';
 
-export default async function AdminLayout({
-  children // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
+export default async function AdminLayout({}: { children: React.ReactNode }) {
   const session = await auth();
-  if (session) {
-    return (
-      <>
-        {session.user?.email}
+  // if (session) {
+  //   return (
+  //     <>
+  //       {session.user?.email}
+  //
+  //       <button
+  //         onClick={async () => {
+  //           'use server';
+  //           await signOut();
+  //         }}
+  //       >
+  //         Sign out
+  //       </button>
+  //     </>
+  //   );
+  // }
+  //
+  return (
+    <div>
+      <ul className="menu bg-base-200 rounded-box w-56">
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Parent</a>
+          <ul>
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul>
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a>Item 3</a>
+        </li>
+      </ul>
 
+      <>
+        Not signed in <br />
         <button
           onClick={async () => {
             'use server';
-            await signOut();
+            await signIn('keycloak');
           }}
         >
-          Sign out
+          Sign in
         </button>
       </>
-    );
-  }
-
-  return (
-    <>
-      Not signed in <br />
-      <button
-        onClick={async () => {
-          'use server';
-          await signIn('keycloak');
-        }}
-      >
-        Sign in
-      </button>
-    </>
+    </div>
   );
 }
