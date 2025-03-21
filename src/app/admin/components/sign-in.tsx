9e933162d8d2ac3726d7febe639c846be5300ced
ドイツ <thd1152015@gmail.com>
2025-03-21 23:11:34 +0700

    Add host configuration to sign-in form; update action to use dynamic redirect URL

--- src/app/admin/components/sign-in.tsx
+++ src/app/admin/components/sign-in.tsx
@@ -3,14 +3,17 @@ import { signIn } from '@/auth';
 export default function SignIn() {
   return (
     <form
-      action={async () => {
+      action={async (formData: FormData) => {
         'use server';
+        const host = formData.get('host') as string;
+        console.log(host);
         await signIn('keycloak', {
           redirect: true,
-          redirectTo: '/admin'
+          redirectTo: `${host}/admin`
         });
       }}
     >
+      <input type="hidden" name="host" value={process.env.NEXT_PUBLIC_HOST || ''} />
       <button type="submit" className="btn btn-accent w-full">
         Signin with Keycloak
       </button>
