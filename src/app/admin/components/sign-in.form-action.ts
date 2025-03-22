1aeb94c2bfc0d5d74b09345fba25af203fde92ec
ドイツ <thd1152015@gmail.com>
2025-03-22 17:56:16 +0700

    Update environment variable for proxy host in sign-in action; replace NEXT_PUBLIC_HOST with PROXY_HOST

--- src/app/admin/components/sign-in.form-action.ts
+++ src/app/admin/components/sign-in.form-action.ts
@@ -3,7 +3,8 @@
 import { signIn } from '@/auth';
 
 export async function signInAction(formData: FormData) {
-  const host = process.env.NEXT_PUBLIC_HOST || '';
+  console.debug('environment: ', process.env.NODE_ENV);
+  const host = process.env.PROXY_HOST || '';
   console.debug('host: ', !host ? 'undefined' : host);
 
   return await signIn('keycloak', {
