815683e75ab61aff149481cdf83eb3da51e3f71b
ドイツ <thd1152015@gmail.com>
2025-03-22 19:07:10 +0700

    Add PROXY_HOST environment variable and update sign-in redirect URL

--- src/app/admin/components/sign-in.form-action.ts
+++ src/app/admin/components/sign-in.form-action.ts
@@ -3,8 +3,10 @@
 import { signIn } from '@/auth';
 
 export async function signInAction(formData: FormData) {
+  const host = process.env.PROXY_HOST || '';
+
   return await signIn('keycloak', {
     redirect: true,
-    redirectTo: `/admin`,
+    redirectTo: `${host}/admin`,
   });
 }
