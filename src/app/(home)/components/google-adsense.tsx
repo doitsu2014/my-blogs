7775ae04be33d62502b2751c20dec70c885ad582
Duc <thd1152015@gmail.com>
2025-03-26 11:44:33 +0700

    Wrap Google Adsense scripts in Head component for improved SEO

--- src/app/(home)/components/google-adsense.tsx
+++ src/app/(home)/components/google-adsense.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
 
 const GoogleAdsense: React.FC = () => {
   return (
-    <>
+    <Head>
       {/* Google tag (gtag.js) */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-6ENZT9HK87"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8576013286531110"
-      crossorigin="anonymous"></script>
-        `
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8576013286531110"
+            crossorigin="anonymous"></script>
+          `
         }}
       />
-    </>
+    </Head>
   );
 };
 
