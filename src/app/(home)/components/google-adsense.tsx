e60b97dd2d72c1dede4696bcabce0d9cf5559792
Duc <thd1152015@gmail.com>
2025-03-26 14:03:53 +0700

    Update Google Adsense script source for improved ad performance

--- src/app/(home)/components/google-adsense.tsx
+++ src/app/(home)/components/google-adsense.tsx
@@ -4,13 +4,13 @@ import Head from 'next/head';
 const GoogleAdsense: React.FC = () => {
   return (
     <Head>
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-6ENZT9HK87"></script>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8576013286531110"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8576013286531110"
-            crossorigin="anonymous"></script>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8576013286531110"></script>
           `
         }}
       />
