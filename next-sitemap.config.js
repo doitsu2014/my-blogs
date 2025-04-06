25b1bd8772f80bf760688fe8671aa95d91fd026f
ドイツ <thd1152015@gmail.com>
2025-04-06 17:32:25 +0700

    Removes next-sitemap
    Removes the next-sitemap package and related configurations.
    The sitemap generation is likely being handled by a different
    mechanism or is no longer required.

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.PROXY_HOST || 'https://ducth.dev',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-  additionalSitemaps: ['https://ducth.dev/additional-sitemap.xml'],
-  exclude: ['/admin'],
-
-};
